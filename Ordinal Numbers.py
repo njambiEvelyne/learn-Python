840963c54c9151d4cfecb700577e2afccfd8c56a
njambiEvelyne <evelynenganga76@gmail.com>
2025-01-03 01:46:41 +0300

    Formula correction

--- Ordinal Numbers.py
+++ Ordinal Numbers.py
@@ -3,9 +3,9 @@ numbers = [1,2,3,4,5,6,7,8,9]
 for number in numbers:
     if number == 1:
         print(f"{number}st")
-    if number ==2 :
+    if number == 2 :
         print(f"{number}nd")
     if number == 3:
         print(f"{number}rd")
-    if number >3 and number <10:
+    if number > 3 and number < 10:
         print(f"{number}th")
