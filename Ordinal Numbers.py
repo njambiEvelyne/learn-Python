d0642ea6d7e11e02f69084a146ef20063be916d1
njambiEvelyne <evelynenganga76@gmail.com>
2025-01-03 01:44:50 +0300

    Added file

---
+++ Ordinal Numbers.py
@@ -0,0 +1,11 @@
+numbers = [1,2,3,4,5,6,7,8,9]
+
+for number in numbers:
+    if number == 1:
+        print(f"{number}st")
+    if number ==2 :
+        print(f"{number}nd")
+    if number == 3:
+        print(f"{number}rd")
+    if number >3 and number <10:
+        print(f"{number}th")
