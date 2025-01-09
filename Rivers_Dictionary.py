706873f11df55be0e91d8fc867020a899a065eca
njambiEvelyne <evelynenganga76@gmail.com>
2025-01-09 09:34:05 +0300

    Project continuation

--- Rivers_Dictionary.py
+++ Rivers_Dictionary.py
@@ -1,4 +1,16 @@
 rivers = {
     "Nile": "Egypt",
-    ""
+    "Amazon": "Brazil",
+    "Yangtze": "China"
 }
+
+for river, country in rivers.items():
+    print(f"{river} runs through {country}")
+print("")
+
+for river in rivers.keys():
+    print(river)
+print("")
+
+for country in rivers.values():
+    print(country)
