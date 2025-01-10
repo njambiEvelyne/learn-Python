436b9a0562e6a07eb89b2cec18dcff5ea0ae00c9
njambiEvelyne <evelynenganga76@gmail.com>
2025-01-10 09:04:57 +0300

    Addeda duplicated file

--- Alx_Tutorial.py
+++ Alx_Tutorial.py
@@ -1,15 +1,17 @@
-def greeting():
-    print("Hello world!")
-
-greeting()
-
-def addition(x,y):
-    result = x + y
-    print(result)
-addition(x= int(input("Enter first number: ")),y= int(input("Enter the second number: ")))
-
-#Lambda Functions
-#Mostly used to perform very simple tasks
-add = lambda a, b: a + b
-res = add(6 ,10)
-print(res)
+# def greeting():
+#     print("Hello world!")
+#
+# greeting()
+#
+# def addition(x,y):
+#     result = x + y
+#     print(result)
+# addition(x= int(input("Enter first number: ")),y= int(input("Enter the second number, y: ")))
+#
+# #Lambda Functions
+# #Mostly used to perform very simple tasks
+# add = lambda a, b: a + b
+# res = add(6 ,10)
+# print(res)
+my_list = [1,2,3,4,"Hello","Hii"]
+print(my_list.reverse())
---
+++ shopping_list_manager.py
@@ -0,0 +1,47 @@
+from shlex import shlex
+
+
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # Prompt for and add an item
+            item = input("Enter the item to be added: ")
+            shopping_list.append(item)
+            print(f"{item} has been added to the list")
+
+            pass
+        elif choice == '2':
+            # Prompt for and remove an item
+            item = input("Enter the name of the item to be removed: ")
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print(f"{item} has been removed from the list")
+
+            else:
+                print(f"'{item}' is not in the list")
+        elif choice == '3':
+            # Display the shopping list
+            if shopping_list:
+                print("Your shopping list:")
+                for i, item in enumerate(shopping_list, start=1):
+                    print(f"{i}. {item}")
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
